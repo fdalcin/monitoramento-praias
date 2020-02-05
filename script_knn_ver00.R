cab2343db3165c7c8e1ebddf4a8318e4b7fa6f8c
Felipe Dalcin <felipe.dalcin@outlook.com>
2020-02-05 17:33:24 -0300

    Trabalhando no modelo KNN

--- script_knn_ver00.R
+++ script_knn_ver00.R
@@ -1,5 +1,10 @@
 library(class)
 library(gmodels)
+library(caret)
+
+accuracy <- function(x) {
+  sum(diag(x) / (sum(rowSums(x)))) * 100
+}
 
 # Carregando dados limpos
 df <-
@@ -12,15 +17,40 @@ df <-
 # Normaliza os dados das colunas com a tabela Z
 df_n <- as.data.frame(scale(df[-1]))
 
-sample_size <- floor(0.75 * nrow(df))
+# Separa o os dados para treino e testes 
+# 70% para treino
+# 30% para testes
+sample_size <- floor(0.7 * nrow(df))
 train_indices <- sample(seq_len(nrow(df)), size = sample_size)
 
 df_train <- df_n[train_indices, ]
 df_test <- df_n[-train_indices, ]
 
-df_train_labels <- df[train_indices, 1]
-df_test_labels <- df[-train_indices, 1]
+train_labels <- df[train_indices, 1]
+test_labels <- df[-train_indices, 1]
 
-df_test_pred <- knn(train = df_train, test = df_test, cl = df_train_labels, k = 21)
+# Calular o número de vizinhos para o modelo
+knn_amount = floor(sqrt(sample_size))
+knn_amount1 = knn_amount + 1
 
-CrossTable(x = df_test_labels, y = df_test_pred, prop.chisq = FALSE)
+# Executa o modelo de KNN
+knn1 <- knn(train = df_train, test = df_test, cl = train_labels, k = knn_amount)
+knn2 <- knn(train = df_train, test = df_test, cl = train_labels, k = knn_amount1)
+
+# Matriz de confusão
+matriz <- table(knn1, test_labels)
+confusionMatrix(matriz)
+accuracy(matriz)
+
+# Matriz de confusão
+matriz <- table(knn2, test_labels)
+confusionMatrix(matriz)
+accuracy(matriz)
+
+# k_optm = 1
+# for (knn_amount in 1:knn_amount1) {
+#   knn_mod <- knn(train = df_train, test = df_test, cl = train_labels, k = knn_amount)
+#   k_optm[knn_amount] <- accuracy(table(knn_mod, test_labels))
+# }
+# 
+# which.max(k_optm)
